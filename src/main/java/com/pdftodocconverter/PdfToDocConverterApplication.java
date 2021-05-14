9612448d1e849f6ab6f3a4bebcffc036cc11f4a1
Nidhish Krishnan <nidhishkrishnan@localhost.localdomain>
2021-05-14 15:47:02 +0530

    checking in

---
+++ src/main/java/com/pdftodocconverter/PdfToDocConverterApplication.java
@@ -0,0 +1,13 @@
+package com.pdftodocconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfToDocConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfToDocConverterApplication.class, args);
+	}
+
+}
